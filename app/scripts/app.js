d728b037f260f971ea1f15c2039a90b75b3e56bc
Jakub Valenta <valenta.jakub@gmail.com>
2014-09-26 19:03:43 +0200

    More trezorService refactoring
    app.js: Split in several functions
    
    ItemStorage: Renamed from DeviceStorage
    
    Transport: Deleted, unnecessary, trezor.js will always be present
    
    DeviceList, deviceService, firmwareService... Documentation

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,150 +1,207 @@
-/*global angular*/
+/*jslint browser: true, devel: true, indent: 4, maxlen: 79 */
 
-'use strict';
+/**
+ * Main
+ *
+ * - Load Transport.
+ * - Bootstrap Angular app.
+ * - Register Bitcoin URI handler.
+ */
+(function (angular) {
+    'use strict';
 
-angular.module('webwalletApp', [
-    'ngRoute',
-    'ngSanitize',
-    'ui.bootstrap',
-    'ja.qr'
-  ]);
+    angular.module('webwalletApp', [
+        'ngRoute',
+        'ngSanitize',
+        'ui.bootstrap',
+        'ja.qr'
+    ]);
 
-// load trezor plugin and bootstrap application
-angular.element(document).ready(function () {
-  var injector = angular.injector(['webwalletApp']),
-      config = injector.get('config');
-
-  var trezor = window.trezor,
-      bridgeUrl = config.bridge.url,
-      bridgeConfigUrl = config.bridge.configUrl,
-      bridgeConfigPromise = trezor.http(bridgeConfigUrl);
-
-  acquireTransport()
-    .then(function (transport) {
-      return bridgeConfigPromise
-        .then(function (config) {
-          return transport.configure(config);
-        })
-        .then(function () {
-          return webwalletApp(null, transport);
-        });
-    })
-    .catch(function (err) {
-      return webwalletApp(err);
+    // Load the Transport and bootstrap the Angular app.
+    angular.element(document).ready(function () {
+        init(
+            loadConfig()
+        );
+        registerUriHandler();
     });
 
-  registerUriHandler();
+    /**
+     * Try to load the Transport and then bootstrap the Angular app.
+     *
+     * @param {Object} config  Configuration
+     *
+     * @see  acquireTransport()
+     * @see  createApp()
+     */
+    function init(config) {
+        var bridgeUrl = config.bridge.url,
+            bridgeConfigUrl = config.bridge.configUrl,
+            bridgeConfigPromise = window.trezor.http(bridgeConfigUrl);
 
-  function acquireTransport() {
-    return loadHttp().catch(loadPlugin);
-
-    function loadHttp() {
-      return trezor.HttpTransport.connect(bridgeUrl)
-        .then(
-          function (info) {
-            console.log('[app] Loading http transport successful', info);
-            return new trezor.HttpTransport(bridgeUrl);
-          },
-          function (err) {
-            console.error('[app] Loading http transport failed', err);
-            throw err;
-          });
+        acquireTransport(bridgeUrl)
+            .then(function (transport) {
+                return bridgeConfigPromise
+                    .then(function (config) {
+                        return transport.configure(config);
+                    })
+                    .then(function () {
+                        return createApp(null, transport);
+                    });
+            })
+            .catch(function (err) {
+                return createApp(err);
+            });
     }
 
-    function loadPlugin() {
-      return trezor.PluginTransport.loadPlugin().then(function (plugin) {
-        return new trezor.PluginTransport(plugin);
-      });
+    /**
+     * Inject and return the config service.
+     *
+     * @return {Object}  Config service
+     */
+    function loadConfig() {
+        var injector = angular.injector(['webwalletApp']);
+        return injector.get('config');
     }
-  }
 
-  function webwalletApp(err, transport) {
-    var app,
-        container = document.getElementById('webwalletApp-container');
+    /**
+     * Acquire Transport
+     *
+     * @param {String} bridgeUrl  Trezor Bridge URL read from the config
+     * @return {Promise}          If the Transport was loaded, the Promise is
+     *                            resolved with an instance of `HttpTransport`
+     *                            or `PluginTransport`.  If loading failed
+     *                            the Promise is failed.
+     */
+    function acquireTransport(bridgeUrl) {
+        var trezor = window.trezor;
 
-    if (!err) {
-      app = initApp();
-    } else {
-      app = initAppError();
+        function loadHttp() {
+            return trezor.HttpTransport.connect(bridgeUrl).then(
+                function (info) {
+                    console.log('[app] Loading http transport successful',
+                        info);
+                    return new trezor.HttpTransport(bridgeUrl);
+                },
+                function (err) {
+                    console.error('[app] Loading http transport failed', err);
+                    throw err;
+                }
+            );
+        }
+
+        function loadPlugin() {
+            return trezor.PluginTransport.loadPlugin().then(function (plugin) {
+                return new trezor.PluginTransport(plugin);
+            });
+        }
+
+        return loadHttp().catch(loadPlugin);
     }
-    app
-      .value('trezorError', err)
-      .value('trezorApi', window.trezor)
-      .value('trezor', transport);
 
-    angular.bootstrap(container, ['webwalletApp']);
-  }
+    /**
+     * Bootstrap (create and initialize) the Angular app.
+     *
+     * Pass to the app the reference to the Transport object.
+     *
+     * @param {Error|null} err    Error from the Transport loading process
+     * @param {Object} transport  Transport object
+     */
+    function createApp(err, transport) {
+        // Create module.
+        var app = angular.module('webwalletApp');
 
-  function initApp() {
-    return angular.module('webwalletApp')
-      .config(function ($routeProvider) {
+        // Attach routes.
+        if (!err) {
+            app.config(attachRoutes);
+        }
+
+        // Pass Transport reference.
+        app
+            .value('trezorError', err)
+            .value('trezorApi', window.trezor)
+            .value('trezor', transport);
+
+        // Initialize Angular.js.
+        angular.bootstrap(
+            document.getElementById('webwalletApp-container'),
+            ['webwalletApp']
+        );
+    }
+
+    /**
+     * Attach routes to passed $routeProvider.
+     *
+     * @param {Object} $routeProvider  Angular $routeProvider as returned
+     *                                 by `app.config()`.
+     */
+    function attachRoutes($routeProvider) {
         $routeProvider
-          .when('/', {
-            templateUrl: 'views/main.html'
-          })
-          .when('/import', {
-            templateUrl: 'views/import.html'
-          })
-          .when('/device/:deviceId', {
-            templateUrl: 'views/device/index.html'
-          })
-          .when('/device/:deviceId/load', {
-            templateUrl: 'views/device/load.html'
-          })
-          .when('/device/:deviceId/recovery', {
-            templateUrl: 'views/device/recovery.html'
-          })
-          .when('/device/:deviceId/wipe', {
-            templateUrl: 'views/device/wipe.html'
-          })
-          .when('/device/:deviceId/account/:accountId', {
-            templateUrl: 'views/account/index.html'
-          })
-          .when('/device/:deviceId/account/:accountId/send', {
-            templateUrl: 'views/account/send.html'
-          })
-          .when('/device/:deviceId/account/:accountId/send/:output', {
-            templateUrl: 'views/account/send.html'
-          })
-          .when('/device/:deviceId/account/:accountId/send/:output/amount/:amount', {
-            templateUrl: 'views/account/send.html'
-          })
-          .when('/device/:deviceId/account/:accountId/receive', {
-            templateUrl: 'views/account/receive.html'
-          })
-          .when('/send/:uri*', {
-            resolve: {
-              uriRedirect: 'uriRedirect'
-            }
-          })
-          .otherwise({
-            redirectTo: '/'
-          });
-      });
-  }
-
-  function initAppError() {
-    return angular.module('webwalletApp');
-  }
-
-  /**
-   * Register Bitcoin URI handler
-   */
-  function registerUriHandler() {
-    var URI_PROTOCOL = 'bitcoin',
-        URI_TEMPLATE = '/#/send/%s',
-        URI_NAME = 'MyTrezor: Send Bitcoins to address',
-        url;
-
-    url = location.protocol + '//' + location.host + URI_TEMPLATE;
-    if (navigator.registerProtocolHandler &&
-        (!navigator.isProtocolHandlerRegistered ||
-         !navigator.isProtocolHandlerRegistered(URI_PROTOCOL, url))) {
-      navigator.registerProtocolHandler(
-        URI_PROTOCOL,
-        url,
-        URI_NAME
-      );
+            .when('/', {
+                templateUrl: 'views/main.html'
+            })
+            .when('/import', {
+                templateUrl: 'views/import.html'
+            })
+            .when('/device/:deviceId', {
+                templateUrl: 'views/device/index.html'
+            })
+            .when('/device/:deviceId/load', {
+                templateUrl: 'views/device/load.html'
+            })
+            .when('/device/:deviceId/recovery', {
+                templateUrl: 'views/device/recovery.html'
+            })
+            .when('/device/:deviceId/wipe', {
+                templateUrl: 'views/device/wipe.html'
+            })
+            .when('/device/:deviceId/account/:accountId', {
+                templateUrl: 'views/account/index.html'
+            })
+            .when('/device/:deviceId/account/:accountId/send', {
+                templateUrl: 'views/account/send.html'
+            })
+            .when('/device/:deviceId/account/:accountId/send/:output', {
+                templateUrl: 'views/account/send.html'
+            })
+            .when('/device/:deviceId/account/:accountId/send/:output/amount/:amount', {
+                templateUrl: 'views/account/send.html'
+            })
+            .when('/device/:deviceId/account/:accountId/receive', {
+                templateUrl: 'views/account/receive.html'
+            })
+            .when('/send/:uri*', {
+                resolve: {
+                    uriRedirect: 'uriRedirect'
+                }
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
     }
-  }
-});
+
+    /**
+     * Register Bitcoin URI handler
+     *
+     * Requests to this URI are then handled by the `uriRedirect` service.
+     *
+     * @see  services/uriRedirect.js
+     */
+    function registerUriHandler() {
+        var URI_PROTOCOL = 'bitcoin',
+            URI_TEMPLATE = '/#/send/%s',
+            URI_NAME = 'MyTrezor: Send Bitcoins to address',
+            url;
+
+        url = location.protocol + '//' + location.host + URI_TEMPLATE;
+        if (navigator.registerProtocolHandler &&
+                (!navigator.isProtocolHandlerRegistered ||
+                 !navigator.isProtocolHandlerRegistered(URI_PROTOCOL, url))) {
+            navigator.registerProtocolHandler(
+                URI_PROTOCOL,
+                url,
+                URI_NAME
+            );
+        }
+    }
+
+}(this.angular));
